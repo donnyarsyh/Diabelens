370d7c41b74efd5d54edfd0e1f068f9c77134f29
itsdrsw <marswan27@gmail.com>
2025-12-31 09:33:20 +0700

    [IMP] Update view Navbar

--- resources/views/page/navbar.blade.php
+++ resources/views/page/navbar.blade.php
@@ -2,15 +2,19 @@
     <div class="container-judul-menu-diabetes">
         @if (Route::currentRouteName() === 'diabetes.page' || Route::currentRouteName() === 'diabetes.predict')
             <h1 class="judul-menu-diabetes">Manajemen Diabetes</h1>
+            <p class="deskripsi__menu-diabetes">Diabelens membantu pasien diabetes memantau kondisi
+                kesehatan dan mengelola pola hidup berdasarkan data pemeriksaan medis dengan dukungan kecerdasan
+                buatan.</p>
         @elseif (Route::currentRouteName() === 'lifestyle.page' || Route::currentRouteName() === 'lifestyle.store')
             <h1 class="judul-menu-diabetes">Cek Gaya Hidup</h1>
+            <p class="deskripsi__menu-diabetes">Fitur ini membantu pasien diabetes mengevaluasi pola makan,
+                aktivitas fisik, dan kebiasaan harian sebagai bagian dari pengelolaan gaya hidup sehat berdasarkan
+                analisis kecerdasan buatan.</p>
         @elseif (Route::currentRouteName() === 'profile.page' || Route::currentRouteName() === 'profile.update')
             <h1 class="judul-menu-diabetes">Profile</h1>
         @endif
 
-        <p class="deskripsi__menu-diabetes">Diabelens membantu pasien diabetes memantau kondisi
-            kesehatan dan mengelola pola hidup berdasarkan data pemeriksaan medis dengan dukungan kecerdasan
-            buatan.</p>
+
     </div>
     <div class="page-menu">
         <div class="menu-item active" onclick="window.location='{{ route('diabetes.page') }}'">
